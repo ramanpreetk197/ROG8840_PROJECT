1edaac506ef26760942753241b0558b38ad4eef4
Glaucia <glauciamelo@gmail.com>
2024-05-10 16:46:59 -0400

    Adding files via upload
    I am adding the start code.

---
+++ PROG8840_Calc/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void printWelcomeMenu();
+void printOptions();
+void add();
+void subtract();
+
+
+void main() {
+
+	printWelcomeMenu();
+
+	printOptions();
+
+	int inputNum;
+
+	printf("Enter operation number: ");
+	scanf_s("%1o", &inputNum);
+
+	switch (inputNum)
+	{
+	case 1:
+		add();
+	case 2:
+		subtract();
+	}
+
+}
+
+void printWelcomeMenu() {
+	printf(" **********************\n");
+	printf("**   Welcome to the   **\n");
+	printf("**   PROG8840 Calculator   **\n");
+	printf(" **********************\n");
+}
+
+void printOptions() {
+	printf("1. Add\n");
+	printf("2. Subtract\n");
+}
+
+void add() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 + num2;
+	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	int num1, num2;
+
+	num1 = 0;
+	num2 = 42;
+
+	int result = num2 / num1;
+}
